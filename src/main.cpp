5a6c34d991c7b684b03c95a6eb91f3706a90c51e
Kory291 <01lukasschaefer@gmail.com>
2025-08-17 17:10:32 +0200

    do some refactoring with functions and add logic to read distance of ultrasound module

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,44 @@
 #include <WiFi.h>
 
 // put function declarations here:
+// sound speed in cm/µS
+# define SOUND_SPEED 0.034
+
 const char* ssid = "foo";
 const char* password = "foo";
-
+const int trigPin = 22;
+const int echoPin = 23;
 
 unsigned long current_time = millis();
 unsigned long previous_time = 0;
 const long timeout_time = 2000;
 
+long duration;
+float distanceCm;
+
 WiFiServer server(80);
 
 String header;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  delay(1000);
+void setup_pins() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
 
+void setup_wifi() {
   WiFi.mode(WIFI_STA); //Optional
-  WiFi.begin(ssid, password);
+
+  int i=0;
   Serial.println("\nConnecting");
+  
+  while(i < 10 && WiFi.status() != WL_CONNECTED) {
+    delay(10);
+    Serial.println(".");
+    i++;
+  }
+  if(WiFi.status() != WL_CONNECTED ) {
+    WiFi.begin(ssid, password);
+  }
 
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
@@ -31,14 +49,19 @@ void setup() {
   Serial.println("\nConnected to the WiFi network");
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
-
-  server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  WiFiClient client = server.available();
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(1000);
 
+  setup_wifi();
+  server.begin();
+  setup_pins();
+}
+
+void serve(WiFiClient client) {
   if (client) {
     current_time = millis();
     previous_time = current_time;
@@ -78,6 +101,26 @@ void loop() {
     Serial.println("Client disconnected.");
     Serial.println("");
   }
+}
+
+float get_distance_measurement() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * SOUND_SPEED / 2;
+  return distanceCm;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  WiFiClient client = server.available();
+  serve(client);
+  distanceCm = get_distance_measurement();
+  Serial.printf("Distance in cm: %f\n", distanceCm);
+  delay(1000);
 }
 
 // put function definitions here:
